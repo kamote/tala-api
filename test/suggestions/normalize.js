6b013c1dd058ca9371a517a2dcc01a01964ec55a
David Blurton <david.blurton@gmail.com>
2016-07-16 11:40:28 +0000

    Add test for single to double letter mapping

--- test/suggestions/normalize.js
+++ test/suggestions/normalize.js
@@ -3,6 +3,7 @@ import assert from 'assert'
 const replacements = {
   á: 'a',
   f: 'v',
+  æ: 'ae',
 }
 
 function normalize(str) {
@@ -47,5 +48,17 @@ describe('Corrections', () => {
 
     // Two letters wrong
     assert.deepEqual(corrections.correct('alvur'), ['álfur'])
+  })
+
+  it('Should suggest corrections for double letters', () => {
+    const corrections = new Corrections()
+    corrections.add('frábært')
+
+    // One letter wrong
+    assert.deepEqual(corrections.correct('frabært'), ['frábært'])
+    assert.deepEqual(corrections.correct('frábaert'), ['frábært'])
+
+    // Two letters wrong
+    assert.deepEqual(corrections.correct('frabaert'), ['frábært'])
   })
 })
